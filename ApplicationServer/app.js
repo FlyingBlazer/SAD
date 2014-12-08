c21cdae35ad7988cf8ffa41f1333a6f90111365f
qinbingchen <qinbingchen@me.com>
2014-12-08 16:29:01 +0800

    Header and footer.

--- ApplicationServer/app.js
+++ ApplicationServer/app.js
@@ -19,7 +19,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(router);
 
