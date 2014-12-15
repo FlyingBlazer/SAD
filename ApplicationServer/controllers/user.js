71f82345faef7225d191a1ae5850e645398eda7e
zhugongpu <zhugongpu@hotmail.com>
2014-12-15 17:04:23 +0800

    Added：Cookie中加入userId

--- ApplicationServer/controllers/user.js
+++ ApplicationServer/controllers/user.js
@@ -48,8 +48,8 @@ exports.onRegister = function (request, response) {
                                             var result = queryString.parse(feedback);
                                             if (result['errcode'] == 0) {//注册成功
                                                 // 跳转到用户个人主页
-
-                                                getUserInfo(username, function (userInfo) {
+                                                var userId = result['userid'];
+                                                getUserInfo(userId, function (userInfo) {
                                                     setCookie(response, userInfo);//将个人信息写入cookie
                                                     // 跳转到user页面
                                                     response.render('user', {
@@ -153,7 +153,7 @@ exports.onLogin = function (request, response) {
                         var result = queryString.parse(feedback);
                         if (result['errcode'] == 0) {//登录成功
 
-                            getUserInfo(result['username'], function (userInfo) {
+                            getUserInfo(result['userid'], function (userInfo) {
                                 setCookie(response, userInfo);//设置cookie
                                 var backURL = request.header('Referer') || '/';
                                 response.redirect(backURL);//重定向到来时的地址
@@ -182,11 +182,11 @@ exports.onLogout = function (request, response) {
 
 // get
 exports.manage = function (request, response) {
-    if (request.method.toLowerCase() == 'get' && getUsernameFromCookie(request) != '') {
+    if (request.method.toLowerCase() == 'get' && getUserIdFromCookie(request) != '') {
         //跳转到管理页面
-        var username = getUsernameFromCookie(request);
+        var userId = getUserIdFromCookie(request);
         //设置用户名、信用等级
-        getUserInfo(username, function (result) {
+        getUserInfo(userId, function (result) {
             response.render('user', {
                 status: result['status'],
                 name: result['name'],
@@ -199,12 +199,12 @@ exports.manage = function (request, response) {
 
 /**
  * 返回用户信息
- * @param username
+ * @param userId
  * @param onSucceedCallback 参数为userInfo
  */
-function getUserInfo(username, onSucceedCallback) {
+function getUserInfo(userId, onSucceedCallback) {
     //从业务服务器检索用户信息
-    forwardRequestGET('/user/' + username + '/info', function (res) {
+    forwardRequestGET('/user/' + userId + '/info', function (res) {
         if (res.errcode == 200) {
             var feedback = '';
 
@@ -228,11 +228,11 @@ function getUserInfo(username, onSucceedCallback) {
 
 // get
 exports.showUserInformation = function (request, response) {
-    if (request.method.toLowerCase() == 'get' && getUsernameFromCookie(request) != '') {
+    if (request.method.toLowerCase() == 'get' && getUserIdFromCookie(request) != '') {
 
-        var username = getUsernameFromCookie(request);
+        var userId = getUserIdFromCookie(request);
         //设置用户名、信用等级
-        getUserInfo(username, function (result) {
+        getUserInfo(userId, function (result) {
             response.render('profile', {
                 username: result['username'],
                 status: result['status'],
@@ -248,9 +248,9 @@ exports.showUserInformation = function (request, response) {
 
 // post
 exports.manageUserInformation = function (request, response) {
-    if (request.method.toLowerCase() == 'post' && getUsernameFromCookie(request) != '') {
+    if (request.method.toLowerCase() == 'post' && getUserIdFromCookie(request) != '') {
 
-        var username = getUsernameFromCookie(request);
+        var userId = getUserIdFromCookie(request);
 
         var postData = '';
         request.on('data', function (chunk) {
@@ -258,7 +258,7 @@ exports.manageUserInformation = function (request, response) {
         });
 
         request.on('end', function () {
-            forwardRequestPOST(queryString.stringify(postData), '/user/' + username + '/update', function (res) {
+            forwardRequestPOST(queryString.stringify(postData), '/user/' + userId + '/update', function (res) {
                 if (res.errcode == 200) {
                     var feedback = '';
                     res.on('data', function (chunk) {
@@ -285,28 +285,21 @@ exports.manageUserInformation = function (request, response) {
 
 // get
 exports.showReservationList = function (request, response) {
-    if (request.method.toLowerCase() == 'get' && getUsernameFromCookie(request) != '') {
+    if (request.method.toLowerCase() == 'get' && getUserIdFromCookie(request) != '') {
 
-        var username = getUsernameFromCookie(request);
+        var userId = getUserIdFromCookie(request);
 
-        var queryStirngURL = queryString.parse(request.url);
-        c
-
-        var params = 'user-id=' + username + '&time=' + queryStirngURL['time'] + '&hosipital-id=' + queryStirngURL['hosipital-id']
-            + '&department-id=' + queryStirngURL['department-id'];
-
-        showReservations(username, params, response);
+        showReservations(userId, response);
     }
 };
 
 /**
  * 显示用户的预约单
- * @param username
- * @param params 带有用户名
+ * @param userId
  * @param response
  */
-function showReservations(username, params, response) {
-    forwardRequestGET('/user/reservation/list/?' + params, function (res) {
+function showReservations(userId, response) {
+    forwardRequestGET('/user/reservation/list/?userid=' + userId, function (res) {
         if (res.errcode == 200) {
             var feedback = '';
             res.on('data', function (chunk) {
@@ -318,7 +311,7 @@ function showReservations(username, params, response) {
                 var result = queryString.parse(feedback);
                 if (result.errcode == 0) {
 
-                    getUserInfo(username, function (userInfo) {
+                    getUserInfo(userId, function (userInfo) {
                         //渲染预约单界面
                         response.render('reservation', {
                             name: userInfo['name'],
@@ -337,12 +330,12 @@ function showReservations(username, params, response) {
 }
 
 /**
- * 从请求中读取cookie
+ * 从请求中读取cookie获得userId
  * @param request
  * @return 返回username
  */
-function getUsernameFromCookie(request) {
-    return queryString.parse(request.header.cookie)['username'];
+function getUserIdFromCookie(request) {
+    return queryString.parse(request.header.cookie)['userId'];
 }
 
 /**
@@ -353,7 +346,7 @@ function getUsernameFromCookie(request) {
  */
 function setCookie(response, userInfo) {
     // 设置完整用户信息
-    response.setHeader('Set-Cookie', ['username=' + userInfo['username'], 'id=' + userInfo['id'], 'name=' + userInfo['name'], 'phone=' + userInfo['phone'], 'email=' + userInfo['email']]);
+    response.setHeader('Set-Cookie', ['userId=' + userInfo['userId'], 'username=' + userInfo['username'], 'userSocialId=' + userInfo['userSocialId'], 'userRealName=' + userInfo['userRealName'], 'userTelephone=' + userInfo['userTelephone']]);
 }
 
 /**
