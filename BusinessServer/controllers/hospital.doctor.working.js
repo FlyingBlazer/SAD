0d1956f64a0f45da286e6850089887c100355bd9
于湛 <me@yuzhan.info>
2014-12-19 18:02:14 +0800

    添加医生排班调整API

---
+++ BusinessServer/controllers/hospital.doctor.working.js
@@ -0,0 +1,19 @@
+exports.getRaw = function(req, res, next) {
+    //
+};
+
+exports.addWeek = function(req, res, next) {
+    //
+};
+
+exports.updateWeek = function(req, res, next) {
+    //
+};
+
+exports.addTemp = function(req, res, next) {
+    //
+};
+
+exports.deleteTemp = function(req, res, next) {
+    //
+};
