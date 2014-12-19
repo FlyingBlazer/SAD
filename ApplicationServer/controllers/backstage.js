18e64c79191fdc628b91e78aeda3bdfb85d04a7e
zhugongpu <zhugongpu@hotmail.com>
2014-12-19 22:28:23 +0800

    添加cookie相关方法

--- ApplicationServer/controllers/backstage.js
+++ ApplicationServer/controllers/backstage.js
@@ -2,22 +2,44 @@
  * Created by renfei on 14/12/6.
  */
 
+/**
+ * 渲染主页
+ * @param request
+ * @param response
+ */
 exports.home = function (request, response) {
-
     response.render('sb_home');
 };
 
+/**
+ * 渲染登录页面
+ * @param request
+ * @param response
+ */
 exports.login = function (request, response) {
-
     response.render('sb_login');
 };
 
-// post
+/**
+ * 转发登录请求
+ * POST
+ * 成功就跳转到/backstage(并写cookie)
+ * 失败跳转到/backstage/login，添加错误提示wrong_credentials
+ * @param request
+ * @param response
+ */
 exports.onLogin = function (request, response) {
 
 };
 
+/**
+ * 登出
+ * 清空cookie，跳转到/login
+ * @param request
+ * @param response
+ */
 exports.logout = function (request, response) {
+
 
     response.redirect('/login');
 };
@@ -79,6 +101,50 @@ exports.editSchedule = function (request, response) {
 // post
 exports.modifyTempWorking = function (request, response) {
 
-    var doctorId = '1';
+    var doctorId = '1';//todo temp
     response.redirect('/backstage/doctor/' + doctorId + '/edit_schedule');
 };
+
+/**
+ * 获得cookie
+ * cookie 中所有的key已经去掉sb_
+ * @param request
+ */
+function getCookie(request) {
+    return {
+        'username': request.cookies.sb_username,
+        'hospitalId': request.cookies.sb_hospitalId,
+        'hospitalName': request.cookies.sb_hospitalName
+    };
+}
+
+
+/**
+ * 在header中设置cookie
+ * cookie内包含完整的用户信息
+ * @param response
+ * @param username
+ * @param hospitalId
+ * @param hospitalName
+ */
+function setCookie(response, username, hospitalId, hospitalName) {
+
+    var options = {
+        expires: new Date(Date.now() + 900000000),
+        path: '/'
+    };
+
+    response.cookie('sb_username', username, options);
+    response.cookie('sb_hospitalId', hospitalId, options);
+    response.cookie('sb_hospitalName', hospitalName, options);
+}
+
+/**
+ * 清空全部cookie
+ * @param response
+ */
+function clearCookie(response) {
+    response.clearCookie('sb_username', {path: '/'});
+    response.clearCookie('sb_hospitalId', {path: '/'});
+    response.clearCookie('sb_hospitalName', {path: '/'});
+}
