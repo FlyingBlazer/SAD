aff1fb37e0cfe301ef13793e773ab4db877b0876
zhugongpu <zhugongpu@hotmail.com>
2014-12-19 13:07:10 +0800

    修改manageUserInformation方法

--- ApplicationServer/controllers/user.js
+++ ApplicationServer/controllers/user.js
@@ -319,19 +319,25 @@ exports.manageUserInformation = function (request, response) {
 
         var callback = function (result) {
             if (result != null) {
-                if (result.code == 0) {
 
-                    getUserInfo(userId, function (userInfo) {
-                        setCookie(response, userInfo);//更新cookie
-                        response.render('profile', {
-                            errorMessage: '更新成功'
-                        });
-                    });
-                } else {
+                var errorMessage = result.code == 0 ? '更新成功' : '修改失败';
+
+                getUserInfo(userId, function (userInfo) {
+                    setCookie(response, userInfo);//更新cookie
                     response.render('profile', {
-                        errorMessage: '修改失败'
+                        username: result.username,
+                        status: result.status,
+                        sid: result.sid,
+                        name: result.name,
+                        phone: result.phone,
+                        email: result.email,
+                        credit: result.credit,
+                        errorMessage: errorMessage
                     });
-                }
+
+                    printLogMessage(errorMessage);
+                });
+
             }
         };
 
