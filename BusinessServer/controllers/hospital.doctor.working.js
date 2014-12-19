e0b0eccd20c3517c5a81143ccc795a4b90cb18c1
于湛 <me@yuzhan.info>
2014-12-19 20:08:49 +0800

    优化代码结构

--- BusinessServer/controllers/hospital.doctor.working.js
+++ BusinessServer/controllers/hospital.doctor.working.js
@@ -1,66 +1,154 @@
 var Errors = require('../lib/errors');
 
 exports.getRaw = function(req, res, next) {
-	var adminId=req.body.adminId;
-	var doctorId=req.params.doctorId;
-	req.models.administrator.get(adminId, function(err, admin) {
-        if(err && err.message != 'Not found') return next(err);
-        var hospital_id=parseInt(admin.name.substr(5, 8), 10);
-        req.db.driver.execQuery(
-            "SELECT hospital.id as id "+
-            "FROM hospital, department, doctor "+
-            "WHERE hospital.id=department.hospital_id "+
-            "AND department.id=doctor.department_id "+
-            "AND doctor.id=? LIMIT 1",
-            [doctorId],
-            function(err, data){
-                if(err && err.message != 'Not found') return next(err);
-                if(!data) {
-                    return next(new Errors.AdminDoctorError("No Such Doctor!"));
-                }
-                if(data[0]['id']!=hospital_id){
-                    return next(new Errors.AdminAccessRejected("Invalid Access For This Administrator!"));
-                }
-                var week_stat=[[0,0,0],[0,0,0],[0,0,0],
-                    [0,0,0],[0,0,0],[0,0,0],[0,0,0]];
-                var temp_stat=[];
-                req.models.working.find({doctor_id: doctorId}, function(err, workings) {
-                    if(err && err.message != 'Not found') return next(err);
-                    workings.forEach(function(working){
-                        switch(working.frequency.charAt(0)){
-                            case '0':
-                                var temp_working={
-                                    date: working.date,
-                                    period: working.period,
-                                    isWorking: working.frequency.charAt(1)=='1'
-                                };
-                                temp_stat.push(temp_working);
-                                break;
-                            case '2':
-                                for(var i = 1 ; i <= 7 ; i++){
-                                    if(working.frequency.charAt(i)=='1'){
-                                        week_stat[i-1][working.period-1]=1;
-                                    }
-                                }
-                                break;
+    check(req, function(data) {
+        var week_stat=[[0,0,0],[0,0,0],[0,0,0],
+            [0,0,0],[0,0,0],[0,0,0],[0,0,0]];
+        var temp_stat=[];
+        req.models.working.find({doctor_id: doctorId}, function(err, workings) {
+            if(err && err.message != 'Not found') return next(err);
+            workings.forEach(function(working){
+                switch(working.frequency.charAt(0)){
+                    case '0':
+                        var temp_working={
+                            date: working.date,
+                            period: working.period,
+                            isWorking: working.frequency.charAt(1)=='1'
+                        };
+                        temp_stat.push(temp_working);
+                        break;
+                    case '2':
+                        for(var i = 1 ; i <= 7 ; i++){
+                            if(working.frequency.charAt(i)=='1'){
+                                week_stat[i-1][working.period-1]=1;
+                            }
                         }
-                    });
-                    res.json({
-                        code: 0,
-                        message: 'success'
-                    });
-                });
+                        break;
+                }
             });
+            res.json({
+                code: 0,
+                message: 'success'
+            });
+        });
     });
 };
 
 exports.addWeek = function(req, res, next) {
+    check(req, function(data) {
+        var new_working=req.body.day;
+        var morning = '20000000';
+        var afternoon = '20000000';
+        var evening = '20000000';
+        for(var i = 0 ; i < 7 ; i++) {
+            morning.replaceAt(i + 1, new_working[i][0]);
+            afternoon.replaceAt(i + 1, new_working[i][1]);
+            evening.replaceAt(i + 1, new_working[i][2]);
+        }
+        var date=new Date();
+        req.models.working.find({doctor_id: doctorId}, function(err, workings) {
+            if(err && err.message != 'Not found') return next(err);
+            if(workings) {
+                return next(new Errors.AddingFailed("Working Arrangement Already Exists!"));
+            }
+            req.models.working.create({
+                doctor_id: doctorId,
+                date: date.Format('yyyy-MM-dd'),
+                period: 1,
+                frequency: morning,
+                price : data[0]['price']
+            }, function(err, working) {
+                if(err && err.message != 'Not found') return next(err);
+            });
+            req.models.working.create({
+                doctor_id: doctorId,
+                date: date.Format('yyyy-MM-dd'),
+                period: 2,
+                frequency: afternoon,
+                price : data[0]['price']
+            }, function(err, working) {
+                if(err && err.message != 'Not found') return next(err);
+            });
+            req.models.working.create({
+                doctor_id: doctorId,
+                date: date.Format('yyyy-MM-dd'),
+                period: 3,
+                frequency: evening,
+                price : data[0]['price']
+            }, function(err, working) {
+                if(err && err.message != 'Not found') return next(err);
+            });
+            res.json({
+                code: 0,
+                message: 'success'
+            });
+        });
+    });
+};
+
+exports.updateWeek = function(req, res, next) {
+    check(req, function(data) {
+        var works=req.body.works;
+        var morning='20000000';
+        var afternoon='20000000';
+        var evening='20000000';
+        req.models.working.find({doctor_id: doctorId}, function(err, workings){
+            if(err && err.message != 'Not found') return next(err);
+            var m_index=0;
+            var a_index=0;
+            var e_index=0;
+            for(var i=0;i<3;i++){
+                switch(workings[i]['period']) {
+                    case 1:
+                        m_index=i;
+                        break;
+                    case 2:
+                        a_index=i;
+                        break;
+                    case 3:
+                        e_index=i;
+                        break;
+                }
+            }
+            for(var i = 0 ; i < works.length ; i++) {
+                switch(works[i]['period']) {
+                    case 0:
+                        workings[m_index].frequency.replaceAt(works[i]['day'], works[i]['action']);
+                        break;
+                    case 1:
+                        workings[a_index].frequency.replaceAt(works[i]['day'], works[i]['action']);
+                        break;
+                    case 2:
+                        workings[e_index].frequency.replaceAt(works[i]['day'], works[i]['action']);
+                        break;
+                }
+            }
+            works.save(function(err) {
+                if(err) throw err;
+                res.json({
+                    code: 0,
+                    message: 'success'
+                });
+            });
+        });
+    });
+};
+
+exports.addTemp = function(req, res, next) {
+    check(req, function(data) {
+        //
+    });
+};
+
+exports.deleteTemp = function(req, res, next) {
+    check(req, function(data) {
+        //
+    });
+};
+
+function check(req, cb) {
     var adminId=req.body.adminId;
-    var doctorId=req.params.doctorId;
-    var new_working=req.body.day;
-    var morning = '20000000';
-    var afternoon = '20000000';
-    var evening = '20000000';
+    var doctorId=req.body.doctorId;
     req.models.administrator.get(adminId, function(err, admin) {
         if(err && err.message != 'Not found') return next(err);
         var hospital_id=parseInt(admin.name.substr(5, 8), 10);
@@ -79,174 +167,7 @@ exports.addWeek = function(req, res, next) {
                 if(data[0]['id']!=hospital_id){
                     return next(new Errors.AdminAccessRejected("Invalid Access For This Administrator!"));
                 }
-                for(var i = 0 ; i < 7 ; i++) {
-                    morning.replaceAt(i + 1, new_working[i][0]==0 ? 0 : 1);
-                    afternoon.replaceAt(i + 1, new_working[i][1]==0 ? 0 : 1);
-                    evening.replaceAt(i + 1, new_working[i][2]==0 ? 0 : 1);
-                }
-                var date=new Date();
-                req.models.working.find({doctor_id: doctorId}, function(err, workings) {
-                    if(err && err.message != 'Not found') return next(err);
-                    if(data) {
-                        return next(new Errors.AddingFailed("Working Arrangement Already Exists!"));
-                    }
-                    req.models.working.create({
-                        doctor_id: doctorId,
-                        date: date.Format('yyyy-MM-dd'),
-                        period: 1,
-                        frequency: morning,
-                        price : data[0]['price']
-                    }, function(err, working) {
-                        if(err && err.message != 'Not found') return next(err);
-                    });
-                    req.models.working.create({
-                        doctor_id: doctorId,
-                        date: date.Format('yyyy-MM-dd'),
-                        period: 2,
-                        frequency: afternoon,
-                        price : data[0]['price']
-                    }, function(err, working) {
-                        if(err && err.message != 'Not found') return next(err);
-                    });
-                    req.models.working.create({
-                        doctor_id: doctorId,
-                        date: date.Format('yyyy-MM-dd'),
-                        period: 3,
-                        frequency: evening,
-                        price : data[0]['price']
-                    }, function(err, working) {
-                        if(err && err.message != 'Not found') return next(err);
-                    });
-                    res.json({
-                        code: 0,
-                        message: 'success'
-                    });
-                });
+                cb(data);
             });
     });
-    //
-};
-
-exports.updateWeek = function(req, res, next) {
-    var adminId=req.body.adminId;
-    var doctorId=req.body.doctorId;
-    var works=req.body.works;
-    var morning='20000000';
-    var afternoon='20000000';
-    var evening='20000000';
-
-    req.models.administrator.get(adminId, function(err, admin) {
-        if(err && err.message != 'Not found') return next(err);
-        var hospital_id=parseInt(admin.name.substr(5, 8), 10);
-        req.db.driver.execQuery(
-            "SELECT hospital.id as id "+
-            "FROM hospital, department, doctor "+
-            "WHERE hospital.id=department.hospital_id "+
-            "AND department.id=doctor.department_id "+
-            "AND doctor.id=? LIMIT 1",
-            [doctorId],
-            function(err, data){
-                if(err && err.message != 'Not found') return next(err);
-                if(!data) {
-                    return next(new Errors.AdminDoctorError("No Such Doctor!"));
-                }
-                if(data[0]['id']!=hospital_id){
-                    return next(new Errors.AdminAccessRejected("Invalid Access For This Administrator!"));
-                }
-                req.models.working.find({doctor_id: doctorId}, function(err, workings){
-                    if(err && err.message != 'Not found') return next(err);
-                    var m_index=0;
-                    var a_index=0;
-                    var e_index=0;
-                    for(var i=0;i<3;i++){
-                        switch(workings[i]['period']) {
-                            case 0:
-                                m_index=i;
-                                break;
-                            case 1:
-                                a_index=i;
-                                break;
-                            case 2:
-                                e_index=i;
-                                break;
-                        }
-                    }
-                    for(var i = 0 ; i < works.length ; i++) {
-                        switch(works[i]['period']) {
-                            case 0:
-                                workings[m_index].frequency.replaceAt(works[i]['day'], works[i]['action']);
-                                break;
-                            case 1:
-                                workings[a_index].frequency.replaceAt(works[i]['day'], works[i]['action']);
-                                break;
-                            case 2:
-                                workings[e_index].frequency.replaceAt(works[i]['day'], works[i]['action']);
-                                break;
-                        }
-                    }
-                    works.save(function(err) {
-                       if(err) throw err;
-                        res.json({
-                            code: 0,
-                            message: 'success'
-                        })
-                    });
-                })
-            });
-    });
-    //
-};
-
-exports.addTemp = function(req, res, next) {
-    var adminId=req.body.adminId;
-    var doctorId=req.body.doctorId;
-    req.models.administrator.get(adminId, function(err, admin) {
-        if(err && err.message != 'Not found') return next(err);
-        var hospital_id=parseInt(admin.name.substr(5, 8), 10);
-        req.db.driver.execQuery(
-            "SELECT hospital.id as id "+
-            "FROM hospital, department, doctor "+
-            "WHERE hospital.id=department.hospital_id "+
-            "AND department.id=doctor.department_id "+
-            "AND doctor.id=? LIMIT 1",
-            [doctorId],
-            function(err, data){
-                if(err && err.message != 'Not found') return next(err);
-                if(!data) {
-                    return next(new Errors.AdminDoctorError("No Such Doctor!"));
-                }
-                if(data[0]['id']!=hospital_id){
-                    return next(new Errors.AdminAccessRejected("Invalid Access For This Administrator!"));
-                }
-
-            });
-    });
-    //
-};
-
-exports.deleteTemp = function(req, res, next) {
-    var adminId=req.body.adminId;
-    var doctorId=req.body.doctorId;
-    req.models.administrator.get(adminId, function(err, admin) {
-        if(err && err.message != 'Not found') return next(err);
-        var hospital_id=parseInt(admin.name.substr(5, 8), 10);
-        req.db.driver.execQuery(
-            "SELECT hospital.id as id "+
-            "FROM hospital, department, doctor "+
-            "WHERE hospital.id=department.hospital_id "+
-            "AND department.id=doctor.department_id "+
-            "AND doctor.id=? LIMIT 1",
-            [doctorId],
-            function(err, data){
-                if(err && err.message != 'Not found') return next(err);
-                if(!data) {
-                    return next(new Errors.AdminDoctorError("No Such Doctor!"));
-                }
-                if(data[0]['id']!=hospital_id){
-                    return next(new Errors.AdminAccessRejected("Invalid Access For This Administrator!"));
-                }
-
-            });
-    });
-    //
-};
+}
