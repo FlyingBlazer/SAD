932e740643837044b463cd905ad60f9e9505bb1c
于湛 <me@yuzhan.info>
2014-12-19 13:44:42 +0800

    将星期格式改为“星期X”

--- BusinessServer/controllers/hospital.doctor.js
+++ BusinessServer/controllers/hospital.doctor.js
@@ -122,9 +122,26 @@ exports.detail = function(req, res, next) {
                 slot.push({
                     date: {
                         year: year,
-                        month: month,
+                        month: month + 1,
                         date: dd.nextDay().getDate(),
-                        day: dd.getDay()
+                        day: (function() {
+                            switch(dd.getDay()) {
+                                case 1:
+                                    return '星期一';
+                                case 2:
+                                    return '星期二';
+                                case 3:
+                                    return '星期三';
+                                case 4:
+                                    return '星期四';
+                                case 5:
+                                    return '星期五';
+                                case 6:
+                                    return '星期六';
+                                case 0:
+                                    return '星期日';
+                            }
+                        })()
                     },
                     slot: {
                         morning: false,
