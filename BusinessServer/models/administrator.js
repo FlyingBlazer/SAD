2591dc9a91b396c24c8d2bfd7a1befe329e71c1a
HeavenDuke <trashlhc@163.com>
2014-12-20 21:22:40 +0800

    更新administrator表对应的orm
    匹配了键名
    
    Signed-off-by: HeavenDuke <trashlhc@163.com>

--- BusinessServer/models/administrator.js
+++ BusinessServer/models/administrator.js
@@ -4,7 +4,7 @@ exports.setup = function(db) {
     return db.define("administrator", {
         id: {type: 'serial'},
         name: {type: 'text', size: 50, unique: true, required: true},
-        password: {type: 'text', size: 50, required: true},
+        password: {type: 'text', size: 50, required: true, mapsTo: 'pass'},
         auth: {type: 'integer', required: true}
     });
 };
