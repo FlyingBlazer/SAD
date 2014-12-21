4ae31dd37d3c1877043cef8349a08c4dd0c82046
heavenduke <trashlhc@163.com>
2014-12-21 14:39:14 +0800

    重大更新
    ①更新working数据表结构，并重新生成数据
    ②更新orm，与数据库匹配
    ③修改doctor.detail，匹配新数据规则
    ④修改doctor.working.getraw，匹配新数据规则
    ⑤修改doctor.working.addtemp，匹配新数据规则
    
    Signed-off-by: heavenduke <trashlhc@163.com>

--- BusinessServer/models/working.js
+++ BusinessServer/models/working.js
@@ -6,6 +6,12 @@ exports.setup = function(db) {
         date: {type: 'date', required: true},
         period: {type: 'integer', required: true},
         frequency: {type: 'text', size: 7, required: true},
-        totalApp: {type: 'integer', required: true, mapsTo: 'total_app'}
+        monday: {type: 'integer', required: true},
+        tuesday: {type: 'integer', required: true},
+        wednesday: {type: 'integer', required: true},
+        thursday: {type: 'integer', required: true},
+        friday: {type: 'integer', required: true},
+        saturday: {type: 'integer', required: true},
+        sunday: {type: 'integer', required: true}
     }).hasOne('doctor', db.models.doctor, {reverse: 'working'});
 };
