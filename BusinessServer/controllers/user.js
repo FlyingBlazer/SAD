b2cad720bcd8cafea815cf1b7b1826f8080b4566
heavenduke <trashlhc@163.com>
2014-12-24 05:57:30 +0800

    修正MD5加密
    Signed-off-by: heavenduke <trashlhc@163.com>

--- BusinessServer/controllers/user.js
+++ BusinessServer/controllers/user.js
@@ -44,6 +44,7 @@ exports.updateInfo = function(req, res, next){
                 return next(new Errors.UpdatePasswordFail("Incorrect password"));
             }
             else if(pass_curr){
+                md5 = crypto.createHash('md5');
                 md5.update("sad"+pass_curr);
                 user.password=md5.digest("hex");
             }
