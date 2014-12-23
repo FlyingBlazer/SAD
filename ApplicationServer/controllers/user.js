428612c86f009b6c3401994ac550d5112e935231
renfeisong <renfei.song@gmail.com>
2014-12-23 22:36:25 +0800

    change user center

--- ApplicationServer/controllers/user.js
+++ ApplicationServer/controllers/user.js
@@ -329,17 +329,17 @@ exports.manageUserInformation = function (request, response) {
 
                 getUserInfo(userId, function (userInfo) {
                     setCookie(response, userInfo);//更新cookie
-                    //response.render('profile', {
-                    //    username: userInfo.username,
-                    //    status: userInfo.status,
-                    //    sid: userInfo.sid,
-                    //    name: userInfo.name,
-                    //    phone: userInfo.phone,
-                    //    email: userInfo.email,
-                    //    credit: userInfo.credit,
-                    //    errorMessage: errorMessage
-                    //});
-                    response.send(errorMessage);
+                    response.render('profile', {
+                        username: userInfo.username,
+                        status: userInfo.status,
+                        sid: userInfo.sid,
+                        name: userInfo.name,
+                        phone: userInfo.phone,
+                        email: userInfo.email,
+                        credit: userInfo.credit,
+                        message: errorMessage
+                    });
+                    //response.send(errorMessage);
 
                     printLogMessage('user info : ' + JSON.stringify(userInfo));
                     printLogMessage(errorMessage);
